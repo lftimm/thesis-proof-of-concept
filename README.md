9e900969f9755588e7297e8ee25323d76719dbca
lftimm <lftimm1902@gmail.com>
2025-10-12 15:08:09 -0300

    now using simple lighting to render a sphere on screen

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Thesis proof of cencept
 
-Proof of concept for my Bachelors' Thesis. Using vertex shadres, simulate how a beam would behave while under a single point load, showing the elastic deform curve.
+Proof of concept for my Bachelors' Thesis. Using vertex shadres, simulate how a concrete beam would behave while under a single point load, showing the elastic deform curve.
     
 ## References
-** https://www.davepagurek.com/blog/realtime-deformation/
+* https://www.davepagurek.com/blog/realtime-deformation/
+* https://github.com/davepagurek/glsl-autodiff
+* https://computergraphics.stackexchange.com/questions/12574/how-to-deform-an-object-in-glsl-processing
+* https://medium.com/@qinzitan/mesh-deformation-study-with-a-sphere-ceee37d47e32
+
+### Keywords for further research
+* Deformation Shader, Tesselation Shader
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 // CONSTANTS
 const width = window.innerWidth;
@@ -11,24 +12,27 @@ const farPlane = 1000;
 // SCENE DEFINITIONS
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(cameraFov,ratio,nearPlane,farPlane);
-camera.position.z = 1;
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
+const camera = new THREE.PerspectiveCamera(cameraFov,ratio,nearPlane,farPlane);
+camera.position.z = 1;
+
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
+
 // SCENE OBJECTS
 const vertexShader = document.getElementById("vertexShader").textContent;
 const fragmentShader = document.getElementById("fragmentShader").textContent;
 
-const geometry = new THREE.PlaneGeometry(1,1);
+// const geometry = new THREE.PlaneGeometry(1,1);
+const geometry = new THREE.IcosahedronGeometry(0.5,32,16);
 
 const material = new THREE.ShaderMaterial({
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
-    uniforms:{}
 });
 
 const object = new THREE.Mesh(geometry,material);
